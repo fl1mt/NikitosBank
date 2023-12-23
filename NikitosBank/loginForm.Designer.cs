b1aeafe05c13707057883fb59638b7c1c68418f8
fl1mt <fl1mtscompilations@yandex.ru>
2023-12-23 17:43:09 +0500

    правка формы

--- NikitosBank/loginForm.Designer.cs
+++ NikitosBank/loginForm.Designer.cs
@@ -28,25 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.entryAccountButton = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.login = new System.Windows.Forms.Label();
             this.loginText = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.passwordText = new System.Windows.Forms.TextBox();
+            this.entryAccountButton = new System.Windows.Forms.Button();
             this.registerAccountButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // entryAccountButton
-            // 
-            this.entryAccountButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.entryAccountButton.Location = new System.Drawing.Point(152, 155);
-            this.entryAccountButton.Name = "entryAccountButton";
-            this.entryAccountButton.Size = new System.Drawing.Size(128, 40);
-            this.entryAccountButton.TabIndex = 0;
-            this.entryAccountButton.Text = "Войти";
-            this.entryAccountButton.UseVisualStyleBackColor = true;
-            this.entryAccountButton.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -60,16 +49,16 @@
             // login
             // 
             this.login.AutoSize = true;
-            this.login.Font = new System.Drawing.Font("Lucida Sans Typewriter", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.login.Location = new System.Drawing.Point(61, 44);
             this.login.Name = "login";
-            this.login.Size = new System.Drawing.Size(85, 27);
+            this.login.Size = new System.Drawing.Size(65, 23);
             this.login.TabIndex = 2;
             this.login.Text = "Логин:";
             // 
             // loginText
             // 
-            this.loginText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginText.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.loginText.Location = new System.Drawing.Point(152, 44);
             this.loginText.Multiline = true;
             this.loginText.Name = "loginText";
@@ -79,32 +68,45 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Lucida Sans Typewriter", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(46, 92);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 27);
+            this.label1.Size = new System.Drawing.Size(77, 23);
             this.label1.TabIndex = 4;
             this.label1.Text = "Пароль:";
             // 
             // passwordText
             // 
-            this.passwordText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordText.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.passwordText.Location = new System.Drawing.Point(152, 88);
             this.passwordText.Multiline = true;
             this.passwordText.Name = "passwordText";
             this.passwordText.Size = new System.Drawing.Size(174, 31);
             this.passwordText.TabIndex = 5;
             // 
+            // entryAccountButton
+            // 
+            this.entryAccountButton.BackColor = System.Drawing.Color.Aquamarine;
+            this.entryAccountButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.entryAccountButton.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.entryAccountButton.Location = new System.Drawing.Point(116, 157);
+            this.entryAccountButton.Name = "entryAccountButton";
+            this.entryAccountButton.Size = new System.Drawing.Size(145, 38);
+            this.entryAccountButton.TabIndex = 78;
+            this.entryAccountButton.Text = "Войти";
+            this.entryAccountButton.UseVisualStyleBackColor = false;
+            // 
             // registerAccountButton
             // 
-            this.registerAccountButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.registerAccountButton.Location = new System.Drawing.Point(298, 167);
+            this.registerAccountButton.BackColor = System.Drawing.Color.Aquamarine;
+            this.registerAccountButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.registerAccountButton.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.registerAccountButton.Location = new System.Drawing.Point(270, 166);
             this.registerAccountButton.Name = "registerAccountButton";
-            this.registerAccountButton.Size = new System.Drawing.Size(117, 28);
-            this.registerAccountButton.TabIndex = 6;
+            this.registerAccountButton.Size = new System.Drawing.Size(145, 29);
+            this.registerAccountButton.TabIndex = 79;
             this.registerAccountButton.Text = "Регистрация";
-            this.registerAccountButton.UseVisualStyleBackColor = true;
-            this.registerAccountButton.Click += new System.EventHandler(this.registerAccountButton_Click);
+            this.registerAccountButton.UseVisualStyleBackColor = false;
             // 
             // LoginForm
             // 
@@ -112,12 +114,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(427, 207);
             this.Controls.Add(this.registerAccountButton);
+            this.Controls.Add(this.entryAccountButton);
             this.Controls.Add(this.passwordText);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.loginText);
             this.Controls.Add(this.login);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.entryAccountButton);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "LoginForm";
@@ -129,13 +131,12 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button entryAccountButton;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label login;
         private System.Windows.Forms.TextBox loginText;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox passwordText;
+        private System.Windows.Forms.Button entryAccountButton;
         private System.Windows.Forms.Button registerAccountButton;
     }
 }
